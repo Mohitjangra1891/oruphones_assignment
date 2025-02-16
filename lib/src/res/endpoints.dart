da683952b1be9c2789771c985925f7e3eab2858c
Mohit jangra <jangramohit1891@gmail.com>
2025-02-16 16:21:20 +0530

    Update endpoints.dart

--- lib/src/res/endpoints.dart
+++ lib/src/res/endpoints.dart
@@ -4,6 +4,5 @@ class Endpoints {
   static const _base = BasePaths.baseUrl;
   static const isLoggedIn = "${BasePaths.baseUrl}/isLoggedIn";
   static const otpValidate = "${BasePaths.baseUrl}/login/otpValidate";
-  static const login = "${BasePaths.baseUrl}/restaurant/login";
-  static const register = "${BasePaths.baseUrl}/restaurant/register";
+
 }
