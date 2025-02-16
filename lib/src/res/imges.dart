46bd4f18547cd299ce0ffea80fd1dfcce8b2429b
Mohit jangra <jangramohit1891@gmail.com>
2025-02-16 16:20:34 +0530

    Update imges.dart

--- lib/src/res/imges.dart
+++ lib/src/res/imges.dart
@@ -1,19 +1,1 @@
 
-
-class AppImages{
-  static  const logo='assets/app_icons/logoo.png';
-  static  const round_logo='assets/app_icons/round_logo.png';
-
-  static const userImage='assets/images/user.png';
-  static const login_bg='assets/images/loginBG.png';
-  static const add_doc_bg='assets/images/add_doc_bg.png';
-  static  const pending_verification_bg='assets/images/pending_verification.png';
-  static  const offline_bg='assets/images/offlinebg.png';
-  static  const online_bg='assets/images/onlinebg.png';
-  static const pizza='assets/images/pizza.png';
-  static const item_added='assets/images/item_added.png';
-  static const hdfc_bank='assets/images/hdfc.png';
-
-  static const profile_rest = 'assets/images/profile2.png';
-}
-
